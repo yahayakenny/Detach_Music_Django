62ddb330dd996c5736f1103f229274e9efd8a62a
yahayakenny <y.hkehinde@yahoo.com>
2020-09-18 09:22:40 +0100

    minor fixes

--- band_app/models.py
+++ band_app/models.py
@@ -12,7 +12,6 @@ class Event(models.Model):
     image2 = models.ImageField(blank = True, null = True)
     artiste2 = models.CharField(max_length=50, blank = True, null = True)
     date2 = models.CharField(max_length=50, blank = True, null = True)
-
     image3 = models.ImageField(blank = True, null = True)
     artiste3 = models.CharField(max_length=50, blank = True, null = True)
     date3 = models.CharField(max_length=50, blank = True, null = True)
@@ -29,7 +28,6 @@ class Tour(models.Model):
     date = models.CharField(max_length=50, blank = True, null = True)
     venue = models.CharField(max_length=50, blank = True, null = True)
     description = models.CharField(max_length=200, blank = True, null = True)
-    slug = models.SlugField()
     price = models.FloatField(default=1)
 
 
